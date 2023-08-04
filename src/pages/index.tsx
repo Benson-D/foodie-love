df9ea35c40ac5d7a053c5dbf10a733e7bf60f713
Dan Benson <dbenson3091@gmail.com>
2023-08-04 18:48:00 -0500

    add recipe cards page display, with dynamic layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
-
+import Image from "next/image";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
 
-  const data = api.recipe.getAll.useQuery();
-  console.log(data, "<====data");
+  const { data } = api.recipe.getAll.useQuery();
+  const recipes = data ?? [];
 
   return (
     <>
@@ -23,7 +23,28 @@ const Home: NextPage = () => {
         </div>
       </nav>
       <main>
-        <section className="grid lg-grid-cols-3">
+        <section className="flex justify-center">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 lg:gap-6 py-6 px-4">
+            {recipes.map((recipe) => (
+                <article key={recipe.id} className="w-full rounded overflow-hidden shadow-lg transform transition duration-500 hover:scale-105 cursor-pointer">
+                  <Image
+                    src={recipe?.recipeImage ?? "/default-image.jpg"} 
+                    alt={recipe?.recipeImage ? recipe.name : "recipe-default-image"} 
+                    width={400} 
+                    height={400} />
+                  <header className="px-6 py-4">
+                    <div className="font-bold text-xl capitalize mb-2">{recipe.name}</div>
+                    <p className="text-gray-600 text-base">Prep Time: {recipe.prepTime}</p>
+                    <p className="text-gray-600 text-base">Cooking Time: {recipe.cookingTime}</p>
+                  </header>
+                  <footer className="px-6 py-2 flex">
+                    <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 ml-auto">
+                      #{recipe.mealType?.toLowerCase()}
+                    </span>
+                  </footer>
+                </article>
+            ))}
+          </div>
 
         </section>
       </main>
