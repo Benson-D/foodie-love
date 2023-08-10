f39c390468356608d5c35e71071cacb4873cefef
Dan Benson <dbenson3091@gmail.com>
2023-08-10 18:54:12 -0500

    updated form inputs with animation similiar to  google

--- src/components/MainModal.tsx
+++ src/components/MainModal.tsx
@@ -59,38 +59,24 @@ const MainModal = (): JSX.Element => {
 									<div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
 									<h3 className="pt-2 text-base font-semibold leading-6 text-gray-900" id="modal-title">General Info</h3>
 									<form className="grid grid-cols-2 gap-9 pb-4">
-										<div className="mt-4 pr-4">
-											<label className="block mb-2" htmlFor="recipe-name"></label>
-											<div className="flex border-b border-emerald-500">
-												<input className="appearance-none border-none w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none transition" 
-													id="recipe-name" 
-													type="text"
-													placeholder="Recipe Name*" />
-											</div>
+										<div className="relative mt-3 pr-4">
+    										<input type="text" id="recipe-name" className="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 border-0 border-b border-gray-300 appearance-none dark:border-gray-400 dark:focus:border-emerald-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+   											 <label htmlFor="recipe-name" className="absolute text-sm text-stone-700 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-emerald-600 peer-focus:dark:text-emerald-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">Recipe Name*</label>
 										</div>
-										<div className="mt-4 pr-4">
-											<label className="block mb-2" htmlFor="meal-type"></label>
-											<div className="flex border-b border-emerald-500 ">
-												<input className="appearance-none border-none w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none transition" 
-													id="meal-type" 
-													type="text"
-													placeholder="Meal Type" />
-											</div>
+										<div className="relative mt-3 pr-4">
+    										<input type="text" id="meal-type" className="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 border-0 border-b border-gray-300 appearance-none dark:border-gray-400 dark:focus:border-emerald-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+   											 <label htmlFor="meal-type" className="absolute text-sm text-stone-700 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-emerald-600 peer-focus:dark:text-emerald-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">Meal Type</label>
 										</div>
-										<div className="mt-4 pr-4">
-											<label className="block mb-2" htmlFor="prep-time"></label>
-											<div className="flex border-b border-emerald-500">
-												<input className="appearance-none border-none w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none transition" id="prep-time" type="number" placeholder="Prep Time"/>
-												<span className="inline-flex items-center px-1 text-sm text-gray-400 ">Minutes</span>
-										</div>
-										</div>
-										<div className="mt-4 pr-4">
-											<label className="block mb-2" htmlFor="cooking-time"></label>
-											<div className="flex border-b border-emerald-500">
-												<input className="appearance-none border-none w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none transition" id="cooking-time" type="number" placeholder="Cooking Time*"/>
-												<span className="inline-flex items-center px-1 text-sm text-gray-400 ">Minutes</span>
-											</div>
-										</div>
+										<div className="relative mt-3 pr-4">
+    										<input type="number" id="prep-time" className="block rounded-t-lg pl-2.5 pb-2.5 pt-5 pr-16 w-full text-sm text-gray-900 border-0 border-b border-gray-300 appearance-none dark:border-gray-400 dark:focus:border-emerald-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+   											 <label htmlFor="prep-time" className="absolute text-sm text-stone-700 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-emerald-600 peer-focus:dark:text-emerald-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">Prep Time</label>
+											<span className="absolute right-2.5 top-5 px-2 text-sm text-gray-400 ">Minutes</span>
+										</div> 
+										<div className="relative mt-3 pr-4">
+    										<input type="number" id="cooking-time" className="block rounded-t-lg pl-2.5 pb-2.5 pt-5 pr-16 w-full text-sm text-gray-900 border-0 border-b border-gray-300 appearance-none dark:border-gray-400 dark:focus:border-emerald-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+   											 <label htmlFor="cooking-time" className="absolute text-sm text-stone-700 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-emerald-600 peer-focus:dark:text-emerald-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">Cooking Time*</label>
+											<span className="absolute right-2.5 top-5 px-2 text-sm text-gray-400 ">Minutes</span>
+										</div> 
 									</form>
 									</div>
 								</div>
