32330436d8d325ff3a69e98655e11268136610a1
Dan Benson <dbenson3091@gmail.com>
2023-08-15 22:28:14 -0500

    added react hook form to multi step process

--- src/features/AddRecipe/AddRecipe.tsx
+++ src/features/AddRecipe/AddRecipe.tsx
@@ -3,9 +3,18 @@ import GeneralInfo from "../forms/Generalnfo";
 import useStep from "~/hooks/useStep";
 import AddIngredients from "../forms/AddIngredients";
 import AddInstructions from "../forms/AddInstructions";
+import { useForm, FormProvider, type SubmitHandler } from "react-hook-form";
+
+interface FormValues  {
+	recipeName: string;
+	mealType: string;
+	prepTime: number;
+	cookingTime: number;
+}
 
 const AddRecipe = (): JSX.Element => {
 	const [step, helpers] = useStep(3);
+	const methods = useForm<FormValues>();
 
 	const {
 		canGoToPreviousStep,
@@ -16,24 +25,30 @@ const AddRecipe = (): JSX.Element => {
 
 	const formBtnLabel = canGoToNextStep ? 'Next' : 'Submit';
 
+	const onSubmit: SubmitHandler<FormValues> = (data) => {
+		console.log(data, "<===data validation")
+	};
+
 	return (
 		<FormContext.Provider value={{step}}>
-			<form>
-				<GeneralInfo />
-				<AddIngredients />
-				<AddInstructions />
-			</form>
-			<footer className="bg-gray-50 py-3 sm:flex sm:flex-row-reverse sm:px-6 md:justify-between px-4">
-				<button type="button" 
-						className="inline-flex w-40 justify-center rounded-md bg-sky-950 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-sky-900 sm:ml-3"
-						onClick={() => nextStep()}>{formBtnLabel}</button>
-				{
-					canGoToPreviousStep && ( 
-					<button type="button" 
-							className="mt-3 inline-flex w-40 justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0"
-							onClick={() => previousStep()}>Back</button>)
-				}		
-			</footer>
+			<FormProvider {...methods}>
+				<form onSubmit={(evt) => void methods.handleSubmit(onSubmit)(evt)}>
+					<GeneralInfo />
+					<AddIngredients />
+					<AddInstructions />
+					<footer className="bg-gray-50 py-3 sm:flex sm:flex-row-reverse sm:px-6 md:justify-between px-4">
+						<button type={canGoToNextStep ? "button" : "submit"} 
+								className="inline-flex w-40 justify-center rounded-md bg-sky-950 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-sky-900 sm:ml-3"
+								onClick={() => nextStep()}>{formBtnLabel}</button>
+						{
+							canGoToPreviousStep && ( 
+							<button type="button" 
+									className="mt-3 inline-flex w-40 justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0"
+									onClick={() => previousStep()}>Back</button>)
+						}		
+					</footer>
+				</form>
+			</FormProvider>
 		</FormContext.Provider>
 	);
 };
--- src/features/forms/Generalnfo.tsx
+++ src/features/forms/Generalnfo.tsx
@@ -1,7 +1,9 @@
 import { useContext } from 'react';
+import { useFormContext } from 'react-hook-form';
 import FormContext from "~/context/FormContext";
 
 const GeneralInfo = () => {
+	const { register } = useFormContext();
 
 	const foodContext = useContext(FormContext); 
 	const pageDisplay = foodContext?.step === 0 ? 'block' : 'hidden'; 
@@ -12,21 +14,37 @@ const GeneralInfo = () => {
 				<h3 className="pt-2 text-base font-semibold leading-6 text-gray-900">General Info</h3>
 				<section className="grid grid-cols-2 gap-9 pb-4">
 					<div className="relative mt-3 pr-4">
-						<input type="text" id="recipe-name" className="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 border-0 border-b border-gray-300 appearance-none dark:border-gray-400 dark:focus:border-emerald-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
-							<label htmlFor="recipe-name" className="absolute text-sm text-stone-700 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-emerald-600 peer-focus:dark:text-emerald-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4" >Recipe Name*</label>
+						<input {...register("recipeName")} 
+							type="text" 
+							id="recipeName" 
+							className="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 border-0 border-b border-gray-300 appearance-none dark:border-gray-400 dark:focus:border-emerald-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" 
+							placeholder=" " />
+							<label htmlFor="recipeName" className="absolute text-sm text-stone-700 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-emerald-600 peer-focus:dark:text-emerald-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4" >Recipe Name*</label>
 					</div>
 					<div className="relative mt-3 pr-4">
-						<input type="text" id="meal-type" className="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 border-0 border-b border-gray-300 appearance-none dark:border-gray-400 dark:focus:border-emerald-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "/>
-							<label htmlFor="meal-type" className="absolute text-sm text-stone-700 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-emerald-600 peer-focus:dark:text-emerald-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">Meal Type</label>
+						<input {...register("mealType")}
+							type="text" 
+							id="mealType" 
+							className="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 border-0 border-b border-gray-300 appearance-none dark:border-gray-400 dark:focus:border-emerald-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" 
+							placeholder=" "/>
+							<label htmlFor="mealType" className="absolute text-sm text-stone-700 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-emerald-600 peer-focus:dark:text-emerald-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">Meal Type</label>
 					</div>
 					<div className="relative mt-3 pr-4">
-						<input type="number" id="prep-time" className="block rounded-t-lg pl-2.5 pb-2.5 pt-5 pr-16 w-full text-sm text-gray-900 border-0 border-b border-gray-300 appearance-none dark:border-gray-400 dark:focus:border-emerald-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "/>
-							<label htmlFor="prep-time" className="absolute text-sm text-stone-700 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-emerald-600 peer-focus:dark:text-emerald-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">Prep Time</label>
+						<input {...register("prepTime")}
+							type="number" 
+							id="prepTime" 
+							className="block rounded-t-lg pl-2.5 pb-2.5 pt-5 pr-16 w-full text-sm text-gray-900 border-0 border-b border-gray-300 appearance-none dark:border-gray-400 dark:focus:border-emerald-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" 
+							placeholder=" "/>
+							<label htmlFor="prepTime" className="absolute text-sm text-stone-700 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-emerald-600 peer-focus:dark:text-emerald-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">Prep Time</label>
 						<span className="absolute right-2.5 top-5 px-2 text-sm text-gray-400 ">Minutes</span>
 					</div> 
 					<div className="relative mt-3 pr-4">
-						<input type="number" id="cooking-time" className="block rounded-t-lg pl-2.5 pb-2.5 pt-5 pr-16 w-full text-sm text-gray-900 border-0 border-b border-gray-300 appearance-none dark:border-gray-400 dark:focus:border-emerald-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "/>
-							<label htmlFor="cooking-time" className="absolute text-sm text-stone-700 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-emerald-600 peer-focus:dark:text-emerald-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">Cooking Time*</label>
+						<input {...register("cookingTime")}
+							type="number" 
+							id="cookingTime" 
+							className="block rounded-t-lg pl-2.5 pb-2.5 pt-5 pr-16 w-full text-sm text-gray-900 border-0 border-b border-gray-300 appearance-none dark:border-gray-400 dark:focus:border-emerald-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" 
+							placeholder=" "/>
+							<label htmlFor="cookingTime" className="absolute text-sm text-stone-700 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-emerald-600 peer-focus:dark:text-emerald-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">Cooking Time*</label>
 						<span className="absolute right-2.5 top-5 px-2 text-sm text-gray-400 ">Minutes</span>
 					</div> 
 				</section>
