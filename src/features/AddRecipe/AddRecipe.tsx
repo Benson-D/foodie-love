c523e3e38f33e68e3a2588992d41f4d2b2c274ab
Dan Benson <dbenson3091@gmail.com>
2023-08-22 21:33:31 -0500

    updated footer styles

--- src/features/AddRecipe/AddRecipe.tsx
+++ src/features/AddRecipe/AddRecipe.tsx
@@ -68,16 +68,16 @@ const AddRecipe = (): JSX.Element => {
 					<GeneralInfo />
 					<AddIngredients />
 					<AddInstructions />
-					<footer className="bg-gray-50 py-3 sm:flex sm:flex-row-reverse sm:px-6 md:justify-between px-4">
-						<button type={canGoToNextStep ? "button" : "submit"} 
-								className="inline-flex w-40 justify-center rounded-md bg-sky-950 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-sky-900 sm:ml-3"
-								onClick={() => nextStep()}>{formBtnLabel}</button>
+					<footer className="bg-gray-50 py-3 flex sm:px-6 justify-between px-4">
 						{
 							canGoToPreviousStep && ( 
 							<button type="button" 
 									className="mt-3 inline-flex w-40 justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0"
 									onClick={() => previousStep()}>Back</button>)
 						}		
+						<button type={canGoToNextStep ? "button" : "submit"} 
+								className={`mt-3 inline-flex w-40 justify-center rounded-md bg-sky-950 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-sky-900 sm:ml-3 ${canGoToPreviousStep ? "" : 'ml-auto'}`}
+								onClick={() => nextStep()}>{formBtnLabel}</button>
 					</footer>
 				</form>
 			</FormProvider>
